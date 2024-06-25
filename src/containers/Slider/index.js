92528a86003bda4f6512ffd3cc947696ca9944c3
yoonju88 <yoonju.t@gmail.com>
2024-06-25 11:57:55 +0200

    Corrected byDateDesc.length, uncaught length in setTimeOut, without dependency on useEffect and error activate radio button based on the slide index

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo} from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -7,21 +7,22 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  console.log(data)
-console.log(byDateDesc)
 
+  const byDateDesc = useMemo(() => {
+    if (!data?.focus) return []; // data?.focus가 없는 경우 빈 배열을 반환합니다.
+    return data.focus.slice().sort((evtA, evtB) => new Date(evtA.date) < new Date(evtB.date) ? -1 : 1);
+  }, [data]); // data가 변경될 때마다 새로 계산합니다.
+
+  // 다음 카드를 설정하는 함수
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
-      5000
-    );
+    setIndex(prevIndex => prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0);
   };
+  // useEffect를 사용하여 다음 카드를 5초마다 변경합니다.
   useEffect(() => {
-    nextCard();
-  });
+    const timer = setTimeout(nextCard, 5000);
+    return () => clearTimeout(timer); // cleanup 함수에서 타이머를 정리합니다.
+  }, [index, byDateDesc]);
+  
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
@@ -45,10 +46,10 @@ console.log(byDateDesc)
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${byDateDesc.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
