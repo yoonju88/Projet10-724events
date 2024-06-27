7d9f457f2a9c2c7754409dcafed8f6ba7173f349
yoonju88 <yoonju.t@gmail.com>
2024-06-27 20:36:20 +0200

    added logic for 'last' on Dataprovider function and added dependency on useEffect

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,24 +19,51 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null)
   const getData = useCallback(async () => {
-    try {
-      setData(await api.loadData());
+    try {  
+      const fetchedData = await api.loadData();   
+      setData(fetchedData);
     } catch (err) {
       setError(err);
     }
-  }, []);
+  }, [setData, setError]);
+
   useEffect(() => {
-    if (data) return;
-    getData();
-  });
-  
+    if (data)return;
+    getData();   
+  },[data, getData]);
+
+  useEffect (() => {
+    if (data && data.events && data.focus){
+      const latestEvent = data.events.reduce((a,b) => (
+        new Date(b.date) > new Date(a.date) ? b : a
+      ), data.events[0]);
+
+      const latestFocus = data.focus.reduce((a,b) => (
+        new Date(b.date) > new Date(a.date) ? b : a
+      ), data.focus[0]);
+
+      if (latestEvent && latestFocus ) {
+        const latestDateEvent = new Date(latestEvent.date)
+        const latestDateFocus = new Date(latestFocus.date)
+          if (latestDateEvent > latestDateFocus ) {
+            setLast(latestEvent)
+          } else if (latestDateEvent < latestDateFocus) {
+            setLast(latestFocus)
+          }
+      }
+    }
+    
+  }, [data])
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
