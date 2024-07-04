23de44ff80ad80033153af4b9f706f5945f69768
yoonju88 <yoonju.t@gmail.com>
2024-07-04 12:41:08 +0200

    Correction contact form

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
-
 import "./style.scss";
 
 export const FIELD_TYPES = {
-  INPUT_TEXT: 1,
+  INPUT_TEXT:  1,
   TEXTAREA: 2,
+  EMAIL: 3,
 };
 
-const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
-  let component;
+const Field = ({ type, label, name, placeholder, value, onChange}) => {
+   let component;
+
   switch (type) {
     case FIELD_TYPES.INPUT_TEXT:
       component = (
@@ -17,25 +18,59 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           name={name}
           placeholder={placeholder}
           data-testid="field-testid"
+          id={name}
+          value = {value}
+          onChange={onChange} 
+          required  
+        />
+      );
+      break;
+      case FIELD_TYPES.EMAIL :
+      component = (
+        <input 
+          type="email"
+          name={name}
+          placeholder={placeholder}
+          data-testid="field-testid"
+          id={name}
+          value = {value}
+          onChange={onChange}
+          autoComplete="off"
+          className="inputEmail"
+          required 
+ 
         />
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" />;
+      component = (
+        <textarea 
+          name={name} 
+          data-testid="field-testid" 
+          placeholder={placeholder}
+          id={name}
+          value = {value}
+          onChange={onChange}
+          required 
+        />
+      );
       break;
     default:
       component = (
         <input
-          type="text"
           name={name}
           placeholder={placeholder}
           data-testid="field-testid"
+          id={name}
+          value = {value}
+          onChange={onChange}
         />
       );
+    break;
   }
   return (
     <div className="inputField">
-      <span>{label}</span>
+      <label htmlFor={name}>{label}</label>
       {component}
     </div>
   );
@@ -46,12 +81,15 @@ Field.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange:PropTypes.func.isRequired,
 };
  Field.defaultProps = {
    label: "",
    placeholder: "",
    type: FIELD_TYPES.INPUT_TEXT,
    name: "field-name",
+   value:"",
  }
 
 export default Field;
--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState} from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
@@ -8,44 +8,109 @@ const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500)
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
+  const [formData, setFormData] = useState({
+    nom: '',
+    prenom: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+   const isEmailValid = (email) => { 
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(email)
+   }
+  const isFormValid = () => {
+  const { nom, prenom, email, message} = formData;
+  return (
+    nom.trim() !== '' &&
+    prenom.trim() !== '' &&
+    email.trim() !== '' &&
+    isEmailValid(email) &&
+    message.trim() !== ''
   );
+ };
+
+ const sendContact = useCallback(
+  async (evt) => {
+    evt.preventDefault();
+    if (!isFormValid()) {
+      return ;
+    } 
+    setSending(true);   
+    // We try to call mockContactApi
+    try {
+      await mockContactApi();
+      setSending(false);
+      setFormData({ 
+        nom: '',
+        prenom: '',
+        email: '',
+        message: ''
+      });       
+      onSuccess()
+    } catch (err) {
+      setSending(false);
+      onError(err);
+    }
+  },
+  [formData, onSuccess, onError]
+);
+
   return (
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+          <Field 
+            placeholder="Nom" 
+            label="Nom" 
+            name="nom"
+            type={FIELD_TYPES.INPUT_TEXT}
+            value ={formData.nom}
+            onChange= {handleChange}
+          />
+          <Field 
+            placeholder="Prénom" 
+            label="Prénom"
+            name="prenom"
+            type={FIELD_TYPES.INPUT_TEXT}
+            value ={formData.prenom}
+            onChange= {handleChange}
+            />
           <Select
             selection={["Personel", "Entreprise"]}
             onChange={() => null}
             label="Personel / Entreprise"
             type="large"
             titleEmpty
+            required  
           />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+          <Field 
+            placeholder="Email" 
+            label="Email"
+            name ="email" 
+            type={FIELD_TYPES.EMAIL}
+            value ={formData.email}
+            onChange= {handleChange}
+          />
+          <Button 
+          type={BUTTON_TYPES.SUBMIT} 
+          disabled={sending}
+          >
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
         <div className="col">
           <Field
-            placeholder="message"
+            placeholder="Message"
             label="Message"
+            name="message"
             type={FIELD_TYPES.TEXTAREA}
+            value ={formData.message}
+            onChange= {handleChange}
           />
         </div>
       </div>
