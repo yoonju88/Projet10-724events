ad82d18e62046f628e1b214cea200e59e2cef58a
yoonju88 <yoonju.t@gmail.com>
2024-07-09 11:07:02 +0200

    Additional integration Test units

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,16 @@
-/**
- * 
- */
+import {getMonth} from './index'
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const date = new Date('2022-01-01')
+            const NameOfMonth = getMonth(date)
+            expect(NameOfMonth).toBe('janvier')
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const date = new Date('2022-07-08')
+            const NameOfMonth = getMonth(date)
+            expect(NameOfMonth).toBe('juillet')
         });
     });
 })
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,9 +1,44 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, getByTestId, render, screen, waitFor} from "@testing-library/react";
 import Home from "./index";
+import { api, DataProvider } from "../../contexts/DataContext";
+
+const data = {
+  events: [
+    {
+      id: 1,
+      type: "soirée entreprise",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "Conférence #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: [
+        "1 espace d’exposition",
+        "1 scéne principale",
+        "2 espaces de restaurations",
+        "1 site web dédié",
+      ],
+    },
+
+    {
+      id: 2,
+      type: "forum",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "Forum #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: ["1 espace d’exposition", "1 scéne principale"],
+    },
+  ],
+};
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
-  
     render(<Home />);
     await screen.findByText("Email");
     await screen.findByText("Nom");
@@ -34,21 +69,95 @@ describe("When Form is created", () => {
       await screen.findByText("Message envoyé !");
     });
   });
-
 });
 
-
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
+  beforeEach(() => {
+    // API 호출을 모킹합니다.
+    jest.spyOn(api, "loadData").mockReturnValue(Promise.resolve(data));
+    
+  });
+  it("a list of events is displayed", async () => {
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+       );   
+    const eventLists = await screen.findByTestId('event-lists');
+    const eventCards = await screen.findAllByTestId('event-card')
+    screen.getByTestId('event-title');
+    expect(eventLists).toBeInTheDocument()
+    expect(eventCards.length).toBe(data.events.length)  
+    data.events.forEach((event, index) => {
+      expect(screen.getByText(event.title)).toBeInTheDocument()
+      expect(screen.getByText(event.type)).toBeInTheDocument()
+      const image = screen.getByAltText(event.title)
+      expect(image).toBeInTheDocument()
+      expect(image).toHaveAttribute("src", data.events[index].cover)
+    }) 
+  }) 
+
+  it("a list a people is displayed", async() => {
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+       ); 
+    const peopleLists = await screen.findByTestId('people-list');
+    expect(peopleLists).toBeInTheDocument()
+    const peopleCards = screen.getAllByTestId("people-card")
+    expect(peopleCards.length).toBe(6)
+    const expectedPeople = [
+      { name: "Samira", position: "CEO", imageAlt: "Samira", imageSrc:"/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png" },
+      { name: "Jean-baptiste", position: "Directeur marketing", imageAlt: "Jean-baptiste",  imageSrc:"/images/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.png" },
+      { name: "Alice", position: "CXO", imageAlt: "Alice", imageSrc:"/images/christina-wocintechchat-com-SJvDxw0azqw-unsplash.png"},
+      { name: "Luís", position: "Animateur", imageAlt: "Luís", imageSrc:"/images/jonas-kakaroto-KIPqvvTOC1s-unsplash.png" },
+      { name: "Christine", position: "VP animation", imageAlt: "Christine", imageSrc:"/images/amy-hirschi-b3AYk8HKCl0-unsplash1.png" },
+      { name: "Isabelle", position: "VP communication", imageAlt: "Isabelle", imageSrc:"/images/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.png" },
+    ];
+
+    peopleCards.forEach ((card, index) => {
+      const { name, position, imageAlt, imageSrc} = expectedPeople[index]
+      expect(screen.getByText(name)).toBeInTheDocument()
+      expect(screen.getByText(position)).toBeInTheDocument()
+      const image = card.querySelector("img")
+      expect(image).toBeInTheDocument()
+      expect(image).toHaveAttribute("src", imageSrc)
+      expect(image).toHaveAttribute("alt", imageAlt)
+      })
+    })
+     
+  it("a footer is displayed", async() => { 
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+       );
+    
+    await screen.findByText("Notre derniére prestation")
+    await screen.findByText("Contactez-nous")
+    await screen.findByText("45 avenue de la République, 75000 Paris")
+    await screen.findByText("01 23 45 67 89")
+    await screen.findByText("contact@724events.com")
+    await screen.findByTestId("footer-desc")
+    const logo = await screen.findByTestId('logo-svg')
+    expect(logo).toBeInTheDocument()
+    expect(logo).toHaveAttribute('xmlns', 'http://www.w3.org/2000/svg')
   })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
-  it("a footer is displayed", () => {
-    // to implement
-  })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+
+  it("an event card, with the last event, is displayed", async() => {
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+       );
+    const eventCardsList = await screen.findAllByTestId('event-card')
+    const lastEvent = eventCardsList.reduce((a,b) => (
+      new Date(b.date) > new Date(a.date) ? b : a
+    ))
+    expect(lastEvent).toBeInTheDocument()
+    expect(lastEvent.querySelector("img")).toBeInTheDocument()
+    expect(lastEvent.querySelector(".EventCard__title")).toBeInTheDocument() 
+    expect(lastEvent.querySelector(".EventCard__month")).toBeInTheDocument()
   })
 });
